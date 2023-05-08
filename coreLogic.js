307b1fa4dbfbc4f29e3c8ece859398fbbb085dcf
soma <soma@koii.network>
2023-05-08 13:32:23 -0300

    update coreLogic, distribution list and submit distribution

--- coreLogic.js
+++ coreLogic.js
@@ -50,25 +50,33 @@ class CoreLogic {
     try {
       console.log('GenerateDistributionList called');
       console.log('I am selected node');
-
-      // Write the logic to generate the distribution list here by introducing the rules of your choice
-
-      /*  **** SAMPLE LOGIC FOR GENERATING DISTRIBUTION LIST ******/
+      console.log('Round', round, 'Task State', _dummyTaskState);
+      // The logic to generate the distribution list here
 
       let distributionList = {};
+      let distributionCandidates = [];
       let taskAccountDataJSON = await namespaceWrapper.getTaskState();
+
       if (taskAccountDataJSON == null) taskAccountDataJSON = _dummyTaskState;
+
+      console.log('Task Account Data', taskAccountDataJSON);
+
       const submissions = taskAccountDataJSON.submissions[round];
       const submissions_audit_trigger =
         taskAccountDataJSON.submissions_audit_trigger[round];
+
       if (submissions == null) {
+
         console.log('No submisssions found in N-2 round');
         return distributionList;
+
       } else {
         const keys = Object.keys(submissions);
         const values = Object.values(submissions);
         const size = values.length;
         console.log('Submissions from last round: ', keys, values, size);
+
+         // Logic for slashing the stake of the candidate who has been audited and found to be false
         for (let i = 0; i < size; i++) {
           const candidatePublicKey = keys[i];
           if (
@@ -76,21 +84,60 @@ class CoreLogic {
             submissions_audit_trigger[candidatePublicKey]
           ) {
             console.log(
-              submissions_audit_trigger[candidatePublicKey].votes,
               'distributions_audit_trigger votes ',
+              submissions_audit_trigger[candidatePublicKey].votes,
             );
             const votes = submissions_audit_trigger[candidatePublicKey].votes;
-            let numOfVotes = 0;
-            for (let index = 0; index < votes.length; index++) {
-              if (votes[index].is_valid) numOfVotes++;
-              else numOfVotes--;
+            if (votes.length === 0) {
+              // slash 70% of the stake as still the audit is triggered but no votes are casted
+              // Note that the votes are on the basis of the submission value
+              // to do so we need to fetch the stakes of the candidate from the task state
+              const stake_list = taskAccountDataJSON.stake_list;
+              const candidateStake = stake_list[candidatePublicKey];
+              const slashedStake = candidateStake * 0.7;
+              distributionList[candidatePublicKey] = -slashedStake;
+              console.log('Candidate Stake', candidateStake);
+            } else {
+              let numOfVotes = 0;
+              for (let index = 0; index < votes.length; index++) {
+                if (votes[index].is_valid) numOfVotes++;
+                else numOfVotes--;
+              }
+
+              if (numOfVotes < 0) {
+                // slash 70% of the stake as the number of false votes are more than the number of true votes
+                // Note that the votes are on the basis of the submission value
+                // to do so we need to fetch the stakes of the candidate from the task state
+                const stake_list = taskAccountDataJSON.stake_list;
+                const candidateStake = stake_list[candidatePublicKey];
+                const slashedStake = candidateStake * 0.7;
+                distributionList[candidatePublicKey] = -slashedStake;
+                console.log('Candidate Stake', candidateStake);
+              }
+
+              if (numOfVotes > 0) {
+                distributionCandidates.push(candidatePublicKey);
+              }
             }
-            if (numOfVotes < 0) continue;
-          }
-          distributionList[candidatePublicKey] = 1;
+            } else {
+              distributionCandidates.push(candidatePublicKey);
+            }
         }
       }
+
+       // now distribute the rewards based on the valid submissions
+      // Here it is assumed that all the nodes doing valid submission gets the same reward
+
+      const reward =
+        taskAccountDataJSON.bounty_amount_per_round /
+        distributionCandidates.length;
+      console.log('REWARD RECEIVED BY EACH NODE', reward);
+      for (let i = 0; i < distributionCandidates.length; i++) {
+        distributionList[distributionCandidates[i]] = reward;
+      }
+
       console.log('Distribution List', distributionList);
+
       return distributionList;
     } catch (err) {
       console.log('ERROR IN GENERATING DISTRIBUTION LIST', err);
@@ -98,7 +145,7 @@ class CoreLogic {
   }
 
   async submitDistributionList(round) {
-    // This function just upload your generated dustribution List and do the transaction for that
+    // This upload the generated dustribution List
 
     console.log('SubmitDistributionList called');
 
@@ -112,36 +159,39 @@ class CoreLogic {
       console.log('DECIDER', decider);
 
       if (decider) {
+
         const response =
           await namespaceWrapper.distributionListSubmissionOnChain(round);
         console.log('RESPONSE FROM DISTRIBUTION LIST', response);
       }
+
     } catch (err) {
       console.log('ERROR IN SUBMIT DISTRIBUTION', err);
     }
   }
 
+
+  // this function is called when a node is selected to validate the submission value
   async validateNode(submission_value, round) {
-    // Write your logic for the validation of submission value here and return a boolean value in response
-
-    // The sample logic can be something like mentioned below to validate the submission
-
-    // try{
-
     console.log('Received submission_value', submission_value, round);
-    // const generatedValue = await namespaceWrapper.storeGet("cid");
-    // console.log("GENERATED VALUE", generatedValue);
-    // if(generatedValue == submission_value){
-    //   return true;
-    // }else{
-    //   return false;
-    // }
-    // }catch(err){
-    //   console.log("ERROR  IN VALDIATION", err);
-    //   return false;
-    // }
 
-    // For succesfull flow we return true for now
+    // import the linktree validate module
+    const vote = await linktree_validate(submission_value, round);
+    console.log('Vote', vote);
+    return vote;
+  }
+
+  async shallowEqual(object1, object2) {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+    for (let key of keys1) {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
     return true;
   }
 
@@ -165,8 +215,6 @@ class CoreLogic {
     _dummyDistributionList,
     _dummyTaskState,
   ) => {
-    // Write your logic for the validation of submission value here and return a boolean value in response
-    // this logic can be same as generation of distribution list function and based on the comparision will final object , decision can be made
 
     try {
       console.log('Distribution list Submitter', distributionListSubmitter);
@@ -180,6 +228,7 @@ class CoreLogic {
       } else {
         fetchedDistributionList = JSON.parse(rawDistributionList);
       }
+
       console.log('FETCHED DISTRIBUTION LIST', fetchedDistributionList);
       const generateDistributionList = await this.generateDistributionList(
         round,
@@ -189,11 +238,7 @@ class CoreLogic {
       // compare distribution list
 
       const parsed = fetchedDistributionList;
-      console.log(
-        'compare distribution list',
-        parsed,
-        generateDistributionList,
-      );
+      console.log('compare distribution list', parsed, generateDistributionList);
       const result = await this.shallowEqual(parsed, generateDistributionList);
       console.log('RESULT', result);
       return result;
@@ -202,6 +247,7 @@ class CoreLogic {
       return false;
     }
   };
+
   // Submit Address with distributioon list to K2
   async submitTask(roundNumber) {
     console.log('submitTask called with round', roundNumber);
@@ -213,6 +259,7 @@ class CoreLogic {
       );
       const submission = await this.fetchSubmission();
       console.log('SUBMISSION', submission);
+      // submit the submission to the K2
       await namespaceWrapper.checkSubmissionAndUpdateRound(
         submission,
         roundNumber,
@@ -224,6 +271,7 @@ class CoreLogic {
   }
 
   async auditTask(roundNumber) {
+
     console.log('auditTask called with round', roundNumber);
     console.log(
       await namespaceWrapper.getSlot(),
